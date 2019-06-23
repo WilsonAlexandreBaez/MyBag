09b6882d68ad1181b3969cf75b0b3e2dc6e4f534
Wilson Alexandre <wab.estacio@gmail.com>
2019-06-23 18:38:16 -0400

    configuração do js e ajuste no localstorag

--- js/Controller/Controller.js
+++ js/Controller/Controller.js
@@ -1,3 +1,41 @@
-function listaConteudo (){
+function listaConteudo() {
     alert("teste ok!")
+
+    let cadastro = PopularPage.buscarTodos()
+    for (let i = 0; i < cadastro.length; i++) {
+        const element = cadastro[i];
+        
+    
+    document.getElementById("mochilas").innerHTML += 
+    /*html */`<div>
+    <div class="img-produto">
+    <img src="${element.img}" alt="${element.nome}">
+    <div class="descricao">
+        <h2>${element.nome}</h2>
+        <p>${element.decricao}</p>
+        <p>${element.preco}</p>
+    </div>
+       
+    </div>`
+}
+
+
+/**
+    let cadasto = PopularPage.buscaProdutos()
+
+    for (let i = 0; i < cadastro.length; i++) {
+        const element = cadastro[i];
+
+     document.getElementById("mochilas").innerHTML +=
+    `
+    <div class="produtos">
+    <img src="${element.img}" alt="${element.nome}">
+    <div class="card-produto-descricao">
+        <h2>${element.nome}</h2>
+        <p>${element.decricao}</p>
+        <p>${element.preco}</p>
+    </div>
+    `
+    }
+    */
 }
--- js/Service/Servicos.js
+++ js/Service/Servicos.js
@@ -1,6 +1,26 @@
-class PopulaPage {
+class PopularPage {
 
-    static listaPrudutos(){
-
+    static validar(mochila) {
+        if (!mochila.nome) {
+            return "O nome é obrigatório."
+        }
     }
+    /**
+     * buscara todos o produtos cadastrados 
+     */
+
+    static buscarTodos() {
+
+        let produtos = JSON.parse(localStorage.getItem("produtos"))
+        if (!produtos) return []
+        else return produtos
+    }
+    
 }
+
+/**
+ *  let produtos = JSON.parse(localStorage.getItem("produtos"))
+        if (!produtos) {
+            return []            
+        }else return produtos
+ */
