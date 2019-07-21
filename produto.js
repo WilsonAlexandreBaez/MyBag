74764c4105e1b59067bd2e39b3646001c8891919
wilsonalexandrebaez <wab.estacio@gmail.com>
2019-07-21 01:24:59 -0400

    cadastro.html

--- produto.js
+++ produto.js
@@ -2,10 +2,7 @@ class produto {
 
     cadastroPrudutos(){
         let nome = " ";
-        let marca = " ";
-        let descricao = " ";
-        let valor = " ";
-        let  img = " ";
+      
 
     }
 }
