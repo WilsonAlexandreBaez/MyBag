128bace40153de3e64c5dfcf6aad1a388a874dcf
wilsonalexandrebaez <wab.estacio@gmail.com>
2019-07-23 16:50:17 -0400

    --- Auto-Git Commit ---

--- js/Controller/CategoriaController.js
+++ js/Controller/CategoriaController.js
@@ -1,8 +1,46 @@
 function masculino() {
-        
+
     let produtos_db = JSON.parse(localStorage.getItem("produtos_db"))
     for (let i = 0; i < produtos_db.length; i++) {
-       
-            console.log(produtos_db[i])
+        if (produtos_db[i].categoria === "Masculino") {
+            produtos_db = produtos_db
+            const element = produtos_db[i];
+            document.getElementById("mochilas").innerHTML +=
+                /*html */
+                `<div>
+    <div class="img-produto">
+    <img src="${element.img}" alt="${element.nome}">
+    </div>
+    <div class="mochilas-descricao">
+        <h2>${element.nome}</h2>
+        <p class="categoria">Categoria:${element.categoria}</p>
+        <p class="descricao">${element.descricao}</p>
+        <p class="preco">${element.preco}</p>
+    </div>
+        `
         }
     }
+}
+function FEmenina() {
+
+    let produtos_db = JSON.parse(localStorage.getItem("produtos_db"))
+    for (let i = 0; i < produtos_db.length; i++) {
+        if (produtos_db[i].categoria === "Masculino") {
+            produtos_db = produtos_db
+            const element = produtos_db[i];
+            document.getElementById("mochilas").innerHTML +=
+                /*html */
+                `<div>
+    <div class="img-produto">
+    <img src="${element.img}" alt="${element.nome}">
+    </div>
+    <div class="mochilas-descricao">
+        <h2>${element.nome}</h2>
+        <p class="categoria">Categoria:${element.categoria}</p>
+        <p class="descricao">${element.descricao}</p>
+        <p class="preco">${element.preco}</p>
+    </div>
+        `
+        }
+    }
+}
