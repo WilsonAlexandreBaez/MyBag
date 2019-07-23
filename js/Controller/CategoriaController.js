600d67cf67712b92480c2869644a6650abc0c7de
wilsonalexandrebaez <wab.estacio@gmail.com>
2019-07-23 17:20:23 -0400

    --- Auto-Git Commit ---

--- js/Controller/CategoriaController.js
+++ js/Controller/CategoriaController.js
@@ -21,11 +21,11 @@ function masculino() {
         }
     }
 }
-function FEmenina() {
+function Feminina() {
 
     let produtos_db = JSON.parse(localStorage.getItem("produtos_db"))
     for (let i = 0; i < produtos_db.length; i++) {
-        if (produtos_db[i].categoria === "Masculino") {
+        if (produtos_db[i].categoria === "Feminina") {
             produtos_db = produtos_db
             const element = produtos_db[i];
             document.getElementById("mochilas").innerHTML +=
@@ -44,3 +44,51 @@ function FEmenina() {
         }
     }
 }
+function Infantil() {
+
+    let produtos_db = JSON.parse(localStorage.getItem("produtos_db"))
+    for (let i = 0; i < produtos_db.length; i++) {
+        if (produtos_db[i].categoria === "Infantil") {
+            produtos_db = produtos_db
+            const element = produtos_db[i];
+            document.getElementById("mochilas").innerHTML +=
+                /*html */
+                `<div>
+        <div class="img-produto">
+        <img src="${element.img}" alt="${element.nome}">
+        </div>
+        <div class="mochilas-descricao">
+            <h2>${element.nome}</h2>
+            <p class="categoria">Categoria:${element.categoria}</p>
+            <p class="descricao">${element.descricao}</p>
+            <p class="preco">${element.preco}</p>
+        </div>
+            `
+        }
+    }
+}
+function Viagem() {
+
+    let produtos_db = JSON.parse(localStorage.getItem("produtos_db"))
+    for (let i = 0; i < produtos_db.length; i++) {
+        if (produtos_db[i].categoria === "Viagem") {
+            produtos_db = produtos_db
+            const element = produtos_db[i];
+            document.getElementById("mochilas").innerHTML +=
+                /*html */
+                `<div>
+        <div class="img-produto">
+        <img src="${element.img}" alt="${element.nome}">
+        </div>
+        <div class="mochilas-descricao">
+            <h2>${element.nome}</h2>
+            <p class="categoria">Categoria:${element.categoria}</p>
+            <p class="descricao">${element.descricao}</p>
+            <p class="preco">${element.preco}</p>
+        </div>
+            `
+        }
+    }
+}
+
+
