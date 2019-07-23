e707515a5967919122a3a654405e766a2933df4f
wilsonalexandrebaez <wab.estacio@gmail.com>
2019-07-23 10:48:47 -0400

    --- Auto-Git Commit ---

--- js/Service/Servicos.js
+++ js/Service/Servicos.js
@@ -19,6 +19,11 @@ class PopularPage {
     static masculino() {
 
         let produtos_db = JSON.parse(localStorage.getItem("produtos_db"))
+        for (let i = 0; i < produtos_db.length; i++) {
+            if (produtos_db.categoria === "Masculino")
+                const produto_db = produtos_db[i];
+
+        }
         if (!produtos_db) return []
         else return produtos_db
     }
